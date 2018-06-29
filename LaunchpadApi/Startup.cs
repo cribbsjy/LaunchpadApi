1c6096ad54f0c81c9f8f86468c151afcf954887a
Joe Cribbs <cribbsjy@gmail.com>
2018-06-28 21:51:40 -0500

    Add nuget packages

--- LaunchpadApi/Startup.cs
+++ LaunchpadApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace LaunchpadApi
 {
@@ -24,6 +25,11 @@ namespace LaunchpadApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Launchpad API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +39,12 @@ namespace LaunchpadApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Launchpad API V1");
+            });
 
             app.UseMvc();
         }
